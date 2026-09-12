eafdcc2705c2c3519558b04197ae0865f78e58a4
Cristian camilo <cristianmilo07@gmail.com>
2026-09-12 10:18:00 -0500

    se realiza todo

--- models/Response.js
+++ models/Response.js
@@ -6,11 +6,19 @@ const answerSchema = new mongoose.Schema({
   value: { type: String, required: true }
 }, { _id: false });
 
+const reflectionSchema = new mongoose.Schema({
+  key: { type: String, required: true },
+  prompt: { type: String, required: true },
+  selected: { type: [String], required: true },
+  note: { type: String, default: '' }
+}, { _id: false });
+
 const responseSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, required: true, index: true },
   username: { type: String, required: true, trim: true, lowercase: true },
   answers: { type: [answerSchema], required: true },
-  submittedAt: { type: Date, default: Date.now }
+  submittedAt: { type: Date, default: Date.now },
+  reflection: { type: [reflectionSchema], default: [] }
 }, { timestamps: true });
 
 module.exports = mongoose.model('SurveyResponse', responseSchema);
